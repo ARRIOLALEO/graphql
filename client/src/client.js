c877bd15a6de29386c30953c689d173e1a0e231b
mario arriola <riblarsv@gmail.com>
2022-08-14 15:18:17 +0600

    added and ended the course

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,49 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import {ApolloLink} from 'apollo-link'
 import gql from 'graphql-tag'
+import { setContext } from 'apollo-link-context'
 
 /**
  * Create a new apollo client and export as default
  */
+
+const typeDefs = gql`{
+    extend type User{
+        age:int,
+    },
+    extend type Pet{
+        vaccinated:string
+    }
+}`
+
+const resolvers = {
+    User:{
+        age(){return 35},
+    },
+    Pet:{
+        vaccinated(){return true}
+    }
+}
+const httpLink = new HttpLink({uri:"http://localhost:4000/"})
+
+const delay =setContext(request =>
+    new Promise((success,fail)=>{
+        setTimeout(()=>{
+            success()
+        },800)
+    })
+)
+const link = ApolloLink.from([delay,httpLink])
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link,
+    cache,
+    typeDefs,
+    resolvers
+})
+
+
+export default client
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -4,20 +4,75 @@ import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
+import {v4 as uuid} from 'uuid'
 
+const PETS_FIELDS =gql`
+fragment PetFields on Pet {
+  id
+  name
+  type
+  img
+  vaccinated @client
+  owner{
+    id
+    age @client
+  }
+}
+`
 
+const ALL_PETS = gql`
+query allPets{
+  pets{
+   ... PetFields
+  }
+}${PETS_FIELDS}
+`
+const ADD_PET=gql`
+mutation addPet($input: NewPetInput!) {
+  addPet(input: $input) {
+    ... PetFields
+  }
+}${PETS_FIELDS}`
 export default function Pets () {
   const [modal, setModal] = useState(false)
 
-
+  const {data,loading ,error} = useQuery(ALL_PETS)
+  const [createPet,petData] = useMutation(ADD_PET,{
+    update(cache,{data:{addPet}}){
+      const {pets} = cache.readQuery({query:ALL_PETS})
+      cache.writeQuery({
+        query:ALL_PETS,
+        data:{pets:[addPet,...pets]}
+      })
+    }
+})
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables:{input: input},
+      optimisticResponse:{
+        __typename:'mutation',
+        addPet:{
+          __typename:'Pet',
+          id:uuid(),
+          name:input.name,
+          type:input.type,
+          img:''
+        }
+      }
+    })
   }
   
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
-
+  if(loading){
+    return <Loader/>
+  }
+  if(error || petData.error){
+    return <p>this is an error</p>
+  }
+  console.log(data.pets[0])
   return (
     <div className="page pets-page">
       <section>
@@ -32,7 +87,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList  pets={data.pets}/>
       </section>
     </div>
   )
